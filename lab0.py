066bb9f5d50f2b05320c11914585b8ff5cbf0ec2
chenyang14 <alexli841114@gmail.com>
2019-09-06 17:26:14 +0800

    init

---
+++ lab0.py
@@ -0,0 +1,17 @@
+def myFunction():
+    '''
+    Write a python function that return the sum of multiples of 3 or 5 between 0 and 100
+    '''
+
+
+
+
+
+
+
+
+    return sum
+
+
+if __name__ == '__main__':
+    myFunction()

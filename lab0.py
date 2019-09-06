6aa9f83d84393d09c434d4b2b75821be6dfa5ca3
chenyang14 <alexli841114@gmail.com>
2019-09-06 17:50:06 +0800

    add comment

--- lab0.py
+++ lab0.py
@@ -1,8 +1,9 @@
 def myFunction():
     '''
     Write a python function that return the sum of multiples of 3 or 5 between 0 and 100
+    And print the answer before you return
+    You can write in python2 or python3 :D
     '''
-
 
 
 
